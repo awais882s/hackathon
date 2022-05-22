9de662db4f6904f0d075839995a3eb245afd3b05
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-05-22 15:02:14 +0500

    cart

---
+++ src/Products/product.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "../Products/style.css";
+import "../Bootstraps/index.css";
+
+export default function product() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
