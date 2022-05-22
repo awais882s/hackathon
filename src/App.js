b149587dfa2672f24aa5f212b0a0317f8ab3baea
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-05-22 07:12:44 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "../src/Bootstraps/index.css"
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        welcome to hackathon
+      </h1>
+    </div>
+  );
+}
+
+export default App;
